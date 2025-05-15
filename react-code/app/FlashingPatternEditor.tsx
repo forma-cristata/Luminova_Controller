bbc28b9b95f5b31d9ae2ca1f16f2fd60da99fc96
forma-cristata <kaaucri@gmail.com>
2025-05-14 20:38:04 -0400

    Flashing pattern page shell created using the same formatting as Color pattern page for consistency...Lots of TODOs to complete functionality here, but this is a good point to roll back to if needed

--- react-code/app/FlashingPatternEditor.tsx
+++ react-code/app/FlashingPatternEditor.tsx
@@ -1,7 +1,10 @@
 import Setting from "@/app/interface/setting-interface";
-import {Dimensions, SafeAreaView, StyleSheet, Text, TouchableOpacity, View} from "react-native";
+import {Dimensions, SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View} from "react-native";
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from '@/app/index';
+import ColorDotsEditorEdition from "@/app/components/ColorDotEditorEdition";
+import Slider from "@react-native-community/slider";
+import React from "react";
 
 
 /**
@@ -14,6 +17,13 @@ import { RootStackParamList } from '@/app/index';
 
 export default function FlashingPatternEditor({ route, navigation }: any) {
     const setting  = route.params?.setting;
+    const [BPM, setBPM] = React.useState(0);
+
+    const calculateBPM = (delayTime: number) : string => {
+        // TODO: Implement the actual calculation for BPM based on delayTime
+
+        return ""
+    }
 
     return (
         <SafeAreaView  style={styles.container}>
@@ -22,13 +32,94 @@ export default function FlashingPatternEditor({ route, navigation }: any) {
                     <Text style={styles.backB}>    ⟨    </Text>
                 </TouchableOpacity>
             </View>
+            <Text style={styles.whiteText}>{setting.name}</Text>
+            {/*<ColorDotsEditorEdition
+                colors={colors}
+                onDotSelect={handleDotSelect}
+                selectedDot={selectedDot}
+                key={colors.join(',')} // Force re-render when colors change
+            />
+
+            TODO: This needs edited to show the animation the same way as in ChooseModification
+
+            */}
+
+            <View style={styles.fpContainer}>
+                <Text style={styles.flashingPatternText}>TODO</Text>
+                {/*<Text style={styles.sliderText}>Hex: #</Text>
+                <TextInput
+                    style={[styles.hexInput]}
+                    value={hexInput}
+                    onChangeText={handleHexInput}
+                    placeholder="FFFFFF"
+                    placeholderTextColor="#666"
+                    maxLength={6}
+                    editable={selectedDot !== null}
+                />
+
+                TODO: This needs edited to display a vertical carousel of the flashing pattern options
+
+                */}
+            </View>
+
+            <View style={styles.sliderContainer}>
+                <View style={styles.sliderRow}>
+                    <Text style={styles.sliderText}>Speed: {calculateBPM(setting.delayTime)} bpm</Text>
+                    <Slider
+                        style={styles.slider}
+                        minimumValue={0}
+                        maximumValue={200}
+                        value={BPM}
+                        /*
+                        TODO:
+                            onValueChange={ value => {
+                            onSlidingComplete={}}*/
+                        minimumTrackTintColor="#ff0000"
+                        maximumTrackTintColor="#ffffff"
+                    />
+                </View>
+
+            </View>
+
+            <View style={styles.buttonContainer}>
+                <View style={styles.buttonRow}>
+                    <TouchableOpacity
+                        style={styles.styleAButton}
+/*
+                        TODO: onPress={}
+*/
+/*
+                        TODO: disabled={}
+*/
+                    >
+                        <Text style={styles.button}>Reset</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity
+                        style={styles.styleAButton}
+                        /*
+                        TODO:
+                            onPress={}
+                            disabled={}
+                        */
+                    >
+                        <Text style={styles.button}>Save</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
         </SafeAreaView>
     );
 }
+
+const { width, height } = Dimensions.get('window');
+const scale = Math.min(width, height) / 375; // Base scale factor
+
+
 const styles=StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: "#000000",
+        alignItems: "center",
     },
     backButton: {
         position: "absolute",
@@ -41,4 +132,84 @@ const styles=StyleSheet.create({
         color: "white",
         fontSize: 30,
     },
+    whiteText: {
+        color: "white",
+        fontSize: 50 * scale,
+        fontFamily: "Thesignature",
+        textAlign: "center",
+        marginTop: height * 0.05,
+        marginBottom: height * 0.03,
+        borderStyle: "solid",
+        borderBottomWidth: 2,
+        borderColor: "white",
+        width: width * 0.8,
+    },
+    flashingPatternText: {
+        color: "white",
+        fontSize: 22 * scale,
+        fontFamily: "Clearlight-lJlq",
+        letterSpacing: 2,
+        padding: 15 * scale,
+
+    },
+    fpContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginTop: height * 0.02,
+        width: width * 0.85,
+        borderStyle: "solid",
+        borderWidth: 2,
+        borderColor: "#ffffff",
+        padding: 10 * scale,
+        borderRadius: 10,
+    },
+    sliderContainer: {
+        width: width * 0.85,
+        marginTop: height * 0.02,
+        borderStyle: "solid",
+        borderWidth: 2,
+        borderColor: "#ffffff",
+        padding: 15 * scale,
+        borderRadius: 10,
+    },
+    sliderRow: {
+        marginVertical: 5 * scale,
+    },
+    slider: {
+        width: "100%",
+        height: 30 * scale,
+    },
+    sliderText: {
+        color: "white",
+        fontSize: 22 * scale,
+        fontFamily: "Clearlight-lJlq",
+        letterSpacing: 2,
+    },
+    buttonContainer: {
+        width: width * 0.85,
+        marginTop: height * 0.02,
+    },
+    buttonRow: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        marginBottom: 8 * scale,
+    },
+    styleAButton: {
+        backgroundColor: "#000000",
+        borderRadius: 10,
+        paddingVertical: 8 * scale,
+        paddingHorizontal: 15 * scale,
+        alignItems: "center",
+        borderStyle: "dashed",
+        borderWidth: 2,
+        borderColor: "#ffffff",
+        width: "48%",
+    },
+    button: {
+        color: "white",
+        fontSize: 30 * scale,
+        fontFamily: "Clearlight-lJlq",
+        letterSpacing: 2,
+
+    }
 });

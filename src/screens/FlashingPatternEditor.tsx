49673d2b2ad5c33a62230132fb0cf4876d2e7cb5
forma-cristata <kaaucri@gmail.com>
2025-09-15 20:48:42 -0400

    ?

--- src/screens/FlashingPatternEditor.tsx
+++ src/screens/FlashingPatternEditor.tsx
@@ -390,7 +390,7 @@ export default function FlashingPatternEditor({
 										: ANIMATION_PATTERNS;
 								const randomPattern =
 									patternsToChooseFrom[
-										Math.floor(Math.random() * patternsToChooseFrom.length)
+									Math.floor(Math.random() * patternsToChooseFrom.length)
 									];
 								setFlashingPattern(randomPattern);
 
